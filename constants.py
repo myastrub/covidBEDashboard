1f0ace974a8ea7b624f6831034a883b07ceec348
Maksym Yastrub <maksymyastrub@Maksyms-Air.fritz.box>
2021-05-15 11:49:33 +0200

    Updated list of constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+#Constants used in the datasets as field names
+#full description of each is available here: 
+#https://epistat.sciensano.be/COVID19BE_codebook.pdf
+
+DATE = 'DATE' 
+PROVINCE = 'PROVINCE'
+REGION = 'REGION'
+AGEGROUP = 'AGEGROUP'
+SEX = 'SEX'
+CASES = 'CASES'
+DEATHS = 'DEATHS'
+TESTS_ALL = 'TESTS_ALL'
+TESTS_ALL_POS = 'TESTS_ALL_POS'
+DOSE = 'DOSE'
+COUNT = 'COUNT'
+CUMUL = 'CUMUL'
+NIS5 = 'NIS5'
+TX_DESCR_FR = 'TX_DESCR_FR'
